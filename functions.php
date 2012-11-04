2308fa24b6721e76053d31b5ea692d693a7f23a1
Carsten Keßler <carsten.kessler@uni-muenster.de>
2012-11-04 23:24:50 +0100

    Home screen icon updated

--- functions.php
+++ functions.php
@@ -139,7 +139,7 @@ function getHead($title = 'WWU Campus Plan'){
 	<link rel="stylesheet" href="css/jquery.mobile-1.2.0.min.css"/>
 	<link rel="stylesheet" href="css/campusplan.css"/>
 	
-	<link rel="apple-touch-icon" href="images/front-az.png"/>
+	<link rel="apple-touch-icon" href="images/start.png"/>
 
 	<script src='js/jquery-1.8.2.min.js'></script>
 	<script src='js/jquery.mobile-1.2.0.min.js'></script>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 	require_once("functions.php");
-	getHead("WWU CampusPlan");
+	getHead("CampusPlan");
 ?>
 
 <body> 
