20faa14f795d8bd4c33bad74aed862ad21e03b69
Carsten Keßler <carsten.kessler@uni-muenster.de>
2013-01-22 12:36:54 +0100

    Info page extended with Link to GitHub project

--- info.php
+++ info.php
@@ -14,15 +14,12 @@
 			<p>Kartendaten &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>. Die daraus erstellten Kacheln werden von <a href="http://www.mapquest.com/">MapQuest</a> zur Verfügung gestellt. Daten und Kacheln stehen unter einer <a href="http://creativecommons.org/licenses/by-sa/2.0/">Creative Commons Lizenz</a>. Die Navigation für Radfahrer, Fußgänger und Autos wird von <a href="http://cloudmade.com/">Cloudmade</a> zur Verfügung gestellt. <!-- Busverbindungen werden von den <a href="https://www.stadtwerke-muenster.de/">Stadtwerken Münster</a> zur Verfügung gestellt. --></p>
 
 			<p>Die Icons stammen von <a href="http://iconmonstr.com/">Iconmonstr</a> (Creative Commons Lizenz) und <a href="http://thenounproject.com/">The Noun Project</a> (Public Domain).</p>
-			
-			<a href="http://www.uni-muenster.de"><img src="img/wwu-full.png" class="logoimg" /></a>
-			<a href="http://ifgi.uni-muenster.de"><img src="img/ifgi.png" class="logoimg" /></a>
 
-						
-
+			<p>Die WWU Campusplan Webapp wird als <a href="">Open Source Projekt</a> am <a href="http://ifgi.uni-muenster.de">Institut für Geoinformatik</a> gepflegt. Der Code befindet sich auf <a href="https://github.com/lodum/Campusplan">GitHub</a>, Verbesserungen können dort per <a href="https://help.github.com/articles/using-pull-requests">Pull Request</a> hinzugefügt werden.</p>			
 		
-	</div><!-- /content -->		
-</div><!-- /page -->
-<a href="https://github.com/lodum/Campusplan"><img style="position: absolute; top: 0; right: 0; border: 0;" src="img/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub" class="hidden-phone"></a>
+			<div class="container hidden-phone"><a href="http://www.uni-muenster.de"><img src="img/wwu-full.png" class="logoimgs pull-right" /></a>
+			<a href="http://ifgi.uni-muenster.de"><img src="img/ifgi.png" class="logoimgs pull-right" /></a></div>	
+		</div> <!-- row -->
+	</div> <!-- content -->
 </body>
 </html>
