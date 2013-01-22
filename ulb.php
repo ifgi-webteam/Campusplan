29fb7c3f77e836388f6a74df3ff59b7661c38f4b
Carsten Keßler <carsten.kessler@uni-muenster.de>
2013-01-22 12:30:04 +0100

    ULB page fixed.

--- ulb.php
+++ ulb.php
@@ -1,25 +1,20 @@
 <?php 
 	require_once("functions.php");
-	getHead("ULB Katalog");
+	getHead();
 ?>
 
-<body> 
-
-<div data-role="page" class="type-interior" id="page">		
-
-	<?php getHeader("ULB Katalog", "home"); ?>
-
-	<div data-role="content">
-		
-		<div class="content-primary">  
-			<iframe src="http://www.ulb.uni-muenster.de/ULB/katalog" width="100%" height="100%" style="min-height: 500px" frameborder="no">
+	<div class="container">
+		<div class="row">
+				
+		<iframe src="http://www.ulb.uni-muenster.de/ULB/katalog" width="100%" height="100%" style="min-height: 500px" frameborder="no">
             <p>Ihr Browser untersützt offenbar keine iFrames. <a href="http://www.ulb.uni-muenster.de/ULB/katalog">Direkt zum ULB Katalog</a>.</p>
-        </iframe> 
-		</div><!--/content-primary -->		
-		
-		<?php getMenu(); ?> 	
+        </iframe> 	
 
-	</div><!-- /content -->		
-</div><!-- /page -->
+		</div>	
 
-<?php getFoot(); ?>
+	</div><!--/container -->
+
+
+<?php
+
+getFoot();
