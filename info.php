b9e23a2b9ec16a49997fa3ed78294a24ab8fab18
Carsten Keßler <carsten.kessler@uni-muenster.de>
2013-01-22 22:53:52 +0100

    Info page tweaked

--- info.php
+++ info.php
@@ -4,21 +4,24 @@
 ?>
 
 	<div class="container">
-		<div class="row">
-						
-			<h1>WWU Campusplan</h1>
-			<p class="lead">Web App Version 1.0 beta <a class="btn btn-info" style="margin-left: 20px" href="mailto:campusplan-support@uni-muenster.de?&amp;subject=Campus%20Plan%20Web%20App%20Feedback">Feedback</a></p>
+		<div class="row-fluid">
+			<div class="span6">		
+				<h1>WWU Campusplan</h1>
+				<p class="lead">Web App Version 1.0 beta <a class="btn btn-info" style="margin-left: 20px" href="mailto:campusplan-support@uni-muenster.de?&amp;subject=Campus%20Plan%20Web%20App%20Feedback">Feedback</a></p>
 
-			<h2>Copyright Hinweise</h2>
-			
-			<p>Kartendaten &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>. Die daraus erstellten Kacheln werden von <a href="http://www.mapquest.com/">MapQuest</a> zur Verfügung gestellt. Daten und Kacheln stehen unter einer <a href="http://creativecommons.org/licenses/by-sa/2.0/">Creative Commons Lizenz</a>. Die Navigation für Radfahrer, Fußgänger und Autos wird von <a href="http://cloudmade.com/">Cloudmade</a> zur Verfügung gestellt. <!-- Busverbindungen werden von den <a href="https://www.stadtwerke-muenster.de/">Stadtwerken Münster</a> zur Verfügung gestellt. --></p>
+				<p>Die WWU Campusplan Webapp wird als <a href="">Open Source Projekt</a> am <a href="http://ifgi.uni-muenster.de">Institut für Geoinformatik</a> gepflegt. Der Code befindet sich auf <a href="https://github.com/lodum/Campusplan">GitHub</a>, Verbesserungen können dort per <a href="https://help.github.com/articles/using-pull-requests">Pull Request</a> hinzugefügt werden.</p>			
 
-			<p>Die Icons stammen von <a href="http://iconmonstr.com/">Iconmonstr</a> (Creative Commons Lizenz) und <a href="http://thenounproject.com/">The Noun Project</a> (Public Domain).</p>
+			</div>
+			<div class="span6">		
+				<h2>Copyright Hinweise</h2>
+				
+				<p>Kartendaten &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>. Die daraus erstellten Kacheln werden von <a href="http://www.mapquest.com/">MapQuest</a> zur Verfügung gestellt. Daten und Kacheln stehen unter einer <a href="http://creativecommons.org/licenses/by-sa/2.0/">Creative Commons Lizenz</a>. Die Navigation für Radfahrer, Fußgänger und Autos wird von <a href="http://cloudmade.com/">Cloudmade</a> zur Verfügung gestellt. <!-- Busverbindungen werden von den <a href="https://www.stadtwerke-muenster.de/">Stadtwerken Münster</a> zur Verfügung gestellt. --></p>
 
-			<p>Die WWU Campusplan Webapp wird als <a href="">Open Source Projekt</a> am <a href="http://ifgi.uni-muenster.de">Institut für Geoinformatik</a> gepflegt. Der Code befindet sich auf <a href="https://github.com/lodum/Campusplan">GitHub</a>, Verbesserungen können dort per <a href="https://help.github.com/articles/using-pull-requests">Pull Request</a> hinzugefügt werden.</p>			
-		
+				<p>Die Icons stammen von <a href="http://iconmonstr.com/">Iconmonstr</a> (Creative Commons Lizenz) und <a href="http://thenounproject.com/">The Noun Project</a> (Public Domain).</p>
+				
+			</div>	
 			<div class="container hidden-phone"><a href="http://www.uni-muenster.de"><img src="img/wwu-full.png" class="logoimgs pull-right" /></a>
-			<a href="http://ifgi.uni-muenster.de"><img src="img/ifgi.png" class="logoimgs pull-right" /></a></div>	
+				<a href="http://ifgi.uni-muenster.de"><img src="img/ifgi.png" class="logoimgs pull-right" /></a></div>			
 		</div> <!-- row -->
 	</div> <!-- container -->
 </div> <!-- content -->
