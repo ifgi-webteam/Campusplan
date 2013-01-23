a00eb28657e724738e2cf97f28d7649d74532507
Carsten Keßler <carsten.kessler@uni-muenster.de>
2013-01-23 10:59:31 +0100

    Info page pimped

--- info.php
+++ info.php
@@ -5,15 +5,22 @@
 
 	<div class="container">
 		<div class="row-fluid">
-			<div class="span6">		
+			<div class="span12">
 				<h1>WWU Campusplan</h1>
-				<p class="lead">Web App Version 1.0 beta <a class="btn btn-info" style="margin-left: 20px" href="mailto:campusplan-support@uni-muenster.de?&amp;subject=Campus%20Plan%20Web%20App%20Feedback">Feedback</a></p>
+			</div>
+		</div>
+		<div class="row-fluid">
+			<div class="span6">		
+				
+				<p class="lead">Web App Version 1.0 beta</p> 
 
-				<p>Die WWU Campusplan Webapp wird als <a href="">Open Source Projekt</a> am <a href="http://ifgi.uni-muenster.de">Institut für Geoinformatik</a> gepflegt. Der Code befindet sich auf <a href="https://github.com/lodum/Campusplan">GitHub</a>, Verbesserungen können dort per <a href="https://help.github.com/articles/using-pull-requests">Pull Request</a> hinzugefügt werden.</p>			
+				<p>Die WWU Campusplan Webapp wird als <a href="">Open Source Projekt</a> am <a href="http://ifgi.uni-muenster.de">Institut für Geoinformatik</a> gepflegt. Der Code befindet sich auf <a href="https://github.com/lodum/Campusplan">GitHub</a>, Verbesserungen können dort per <a href="https://help.github.com/articles/using-pull-requests">Pull Request</a> hinzugefügt werden.</p>
 
+				<p class="lead" align="center" style="margin: 30px 0 30px"><a class="btn btn-info btn-large" href="mailto:campusplan-support@uni-muenster.de?&amp;subject=Campus%20Plan%20Web%20App%20Feedback">Feedback</a></p>
+				
 			</div>
 			<div class="span6">		
-				<h2>Copyright Hinweise</h2>
+				<p class="lead">Copyright Hinweise</p>
 				
 				<p>Kartendaten &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>. Die daraus erstellten Kacheln werden von <a href="http://www.mapquest.com/">MapQuest</a> zur Verfügung gestellt. Daten und Kacheln stehen unter einer <a href="http://creativecommons.org/licenses/by-sa/2.0/">Creative Commons Lizenz</a>. Die Navigation für Radfahrer, Fußgänger und Autos wird von <a href="http://cloudmade.com/">Cloudmade</a> zur Verfügung gestellt. <!-- Busverbindungen werden von den <a href="https://www.stadtwerke-muenster.de/">Stadtwerken Münster</a> zur Verfügung gestellt. --></p>
 
