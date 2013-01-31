7fb664f3cce6e65cfdb7bbd405f46b6f255702fd
Carsten Keßler <carsten.kessler@uni-muenster.de>
2013-01-31 13:24:13 +0100

    Info page updated, img folder cleaned up

--- info.php
+++ info.php
@@ -22,13 +22,14 @@
 			<div class="span6">		
 				<p class="lead">Copyright Hinweise</p>
 				
-				<p>Kartendaten &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>. Die daraus erstellten Kacheln werden von <a href="http://www.mapquest.com/">MapQuest</a> zur Verfügung gestellt. Daten und Kacheln stehen unter einer <a href="http://creativecommons.org/licenses/by-sa/2.0/">Creative Commons Lizenz</a>. Die Navigation für Radfahrer, Fußgänger und Autos wird von <a href="http://cloudmade.com/">Cloudmade</a> zur Verfügung gestellt. <!-- Busverbindungen werden von den <a href="https://www.stadtwerke-muenster.de/">Stadtwerken Münster</a> zur Verfügung gestellt. --></p>
+				<p>Kartendaten &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>. Die daraus erstellten Kacheln werden von <a href="http://www.mapquest.com/">MapQuest</a> zur Verfügung gestellt. Daten und Kacheln stehen unter einer <a href="http://creativecommons.org/licenses/by-sa/2.0/">Creative Commons Lizenz</a>. Die Navigation für Radfahrer, Fußgänger und Autos wird von <a href="http://cloudmade.com/">Cloudmade</a> zur Verfügung gestellt.</p>
+
+				<p>Die App basiert auf offenen Daten, die im Rahmen der <a href="http://lodum.de">Linked Open Data University of Münster</a> Initiative auf <a href="http://data.uni-muenster.de">data.uni-muenster.de</a> zur Verfügung gestellt werden.</p>
 
 				<p>Die Icons stammen von <a href="http://iconmonstr.com/">Iconmonstr</a> (Creative Commons Lizenz) und <a href="http://thenounproject.com/">The Noun Project</a> (Public Domain).</p>
 				
 			</div>	
-			<div class="container hidden-phone"><a href="http://www.uni-muenster.de"><img src="img/wwu-full.png" class="logoimgs pull-right" /></a>
-				<a href="http://ifgi.uni-muenster.de"><img src="img/ifgi.png" class="logoimgs pull-right" /></a></div>			
+			<div class="container"><p align="center"><a href="http://www.uni-muenster.de"><img src="img/wwu-full.png" class="logoimgs" /></a> <a href="http://lodum.de"><img src="img/lodum.png" class="logoimgs" /></a> <a href="http://ifgi.uni-muenster.de"><img src="img/ifgi.png" class="logoimgs" /></a></p></div>
 		</div> <!-- row -->
 	</div> <!-- container -->
 </div> <!-- content -->
