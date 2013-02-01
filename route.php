c7dc687b5c60568f515fe74f18434ee1ba1761d9
Carsten Keßler <carsten.kessler@uni-muenster.de>
2013-02-01 21:10:08 +0100

    Routing prxy script added

---
+++ route.php
@@ -0,0 +1,15 @@
+<?php 
+	
+	// returns a JSON object containing the route itself (to put on the map), plus the instructions
+	// sample call: route.php?coords=47.25976,9.58423,47.2603,9.588&mode=car&lang=de
+
+	include_once('keys.php');
+
+	$url = 'http://routes.cloudmade.com/'.$cloudmadekey.'/api/0.3/'.$_GET["coords"].'/'.$_GET["mode"].'.js?lang='.$_GET["lang"];
+
+	error_log($url);
+
+	header('Content-type: application/json; charset=utf-8');
+	echo file_get_contents($url);
+	
+?>
